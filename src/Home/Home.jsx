27eb7b544aa05dc59fd07ec0761bdd41e1d22370
abdur rhaman babu <abdurrahmanbab74@gamil.com>
2024-04-05 21:34:50 +0600

    create react router

---
+++ src/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Header from "../Header/Header";
+
+const Home = () => {
+    return (
+        <div>
+            <h1>This is home page</h1>
+            <Header/>
+        </div>
+    );
+};
+
+export default Home;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Contact from './Contact/Contact'
+
+
+const router = createBrowserRouter([
+      {
+        path:'/contact',
+        element: <Contact/>
+      }
+  
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router = {router}/>
   </React.StrictMode>,
 )
